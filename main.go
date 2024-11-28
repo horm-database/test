6574f205f3fbfc3daeb61843ba8e1d0b5511e233
caohao <18500482693@163.com>
2024-11-28 22:03:10 +0800

    optimize
    optimize

--- main.go
+++ main.go
@@ -12,12 +12,13 @@ func main() {
 
 // init 配置全局统一接入协议执行器
 func init() {
-	horm.SetGlobalClient("default/app.server.service1",
-		horm.WithAppID(10002),
-		horm.WithSecret("S959223456"),
-		horm.WithTarget("ip://127.0.0.1:8180"),
-		horm.WithTimeout(332320),
-		horm.WithCaller("app.server.service"))
+	horm.SetGlobalClient("ws_test.app1.server1.service1")
+
+	//horm.SetGlobalClient("", horm.WithAppID(10002),
+	//	horm.WithSecret("S959223456"),
+	//	horm.WithTarget("ip://127.0.0.1:8180"),
+	//	horm.WithTimeout(332320),
+	//	horm.WithCaller("app.server.service"))
 
 	//horm.SetGlobalProxy("10002", "S959223456", "polaris://rpc.server.access.goapi", 332320)
 }
